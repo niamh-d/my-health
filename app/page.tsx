9dc8f219d232d52bccfbd0c290208541c8cc036c
Niamh <niamh.linguist@gmail.com>
2024-11-02 15:58:31 +0200

    updated package json

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -8,5 +9,5 @@ export default function Home() {
         &copy; 2024 niamh
       </footer>
     </div>
-  );
+  )
 }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,17 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import pluginJest from 'eslint-plugin-jest'
 
 export default [
   {
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: { jest: pluginJest },
   },
   {
     languageOptions: {
       globals: globals.browser,
+      globals: pluginJest.environments.globals.globals,
     },
   },
   pluginJs.configs.recommended,
@@ -25,6 +28,11 @@ export default [
       '@typescript-eslint/explicit-function-return-type': 'warn',
       'no-undef': 'error',
       'no-unreachable': 'error',
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
     },
   },
 ]
