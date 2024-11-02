b42c3060310393c69cfe04635d6932f69f25f443
Niamh <niamh.linguist@gmail.com>
2024-11-02 15:15:50 +0200

    jest setup and simple tests running

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import Home from "@/app/page";
+
+describe("Home", () => {
+  it("should have Hello text", () => {
+    render(<Home />);
+    const helloText = screen.getByText(/hello/i);
+    expect(helloText).toBeInTheDocument();
+  });
+
+  it('should contain the text "niamh"', () => {
+    render(<Home />);
+    const niamhText = screen.getByText(/niamh/i);
+    expect(niamhText).toBeInTheDocument();
+  });
+});
